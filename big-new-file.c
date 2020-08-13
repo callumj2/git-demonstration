fba3bf9c773b5a26911f65ba0369618880ee84c0
Callum Johnson <callumj2@student.unimelb.edu.au>
2020-08-13 13:23:31 +1000

    Added vscode framework for testing

---
+++ big-new-file.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+    int a,b;
+    printf("Type 2 numbers: ");
+    scanf("%d %d", a,b);
+    printf("The addition of these numbers is: %d", (a + b));
+    return 0;
+}
